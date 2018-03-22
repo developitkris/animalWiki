bc215a7dfc7ddd8835171bd61aff83b0c0dba829
Jim Palowski and Kristi Hwang <kristi@>
2018-03-22 10:40:17 -0700

    Add toggle functions to image text to display animal bio

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  $("#orca").click(function(){
+    $("#orca-text").fadeToggle();
+  });
+
+  $("#spider").click(function(){
+    $("#tarantula-text").fadeToggle();
+  });
+
+  $("#turtle").click(function(){
+    $("#turtle-text").fadeToggle();
+  });
+});
